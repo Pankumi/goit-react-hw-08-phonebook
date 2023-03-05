ee106fbea834198b45aeff2e6ee1534a017dcb29
Pankumi <111710215+Pankumi@users.noreply.github.com>
2023-03-05 16:55:50 +0200

    Revert "Saved"
    This reverts commit ff04ef2a1cf1b575dc3a71343dd2b5554349d94a.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'components/App/App';
+import { App } from 'components/App';
 import './index.css';
-import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/store';
-import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <BrowserRouter basename="/PhoneBook">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
--- src/redux/user/selectors.js
+++
@@ -1,3 +0,0 @@
-export const selectUser = state => state.user.user;
-export const selectIsLoggedIn = state => state.user.isLoggedIn;
-export const selectIsRefreshing = state => state.user.isRefreshing;
