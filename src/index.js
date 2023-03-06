cc0ec052c04e5ec6d3dcbb9e6e2113198a608f4e
Pankumi <111710215+Pankumi@users.noreply.github.com>
2023-03-06 07:52:47 +0200

    <BrowserRouter basename="/goit-react-hw-08-phonebook/">

--- src/index.js
+++ src/index.js
@@ -17,8 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        {/* <BrowserRouter basename="/goit-react-hw-08-phonebook"> */}
-        <BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
           <App />
         </BrowserRouter>
       </PersistGate>
